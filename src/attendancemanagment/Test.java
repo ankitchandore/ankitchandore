feafa23bfd40bebead05e6548c69bcfbfa1b63ea
ankitchandore <ankitchandore9@gmail.com>
2022-04-15 18:46:47 +0530

    welcome change

--- src/attendancemanagment/Test.java
+++ src/attendancemanagment/Test.java
@@ -2,7 +2,7 @@ package attendancemanagment;
 
 public class Test {
 	public static void main(String args[]) {
-		System.out.println("Hello Ankit");
+		System.out.println("Hello Ankit welcome");
 	}
 
 }
